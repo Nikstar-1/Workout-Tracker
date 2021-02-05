aa073b4bdd676f66377506d8c2acd79d8f980272
Nikstar-1 <rk_787@hotmail.co.uk>
2021-02-05 16:27:19 +0000

    deploying

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const morgan = require('morgan')
+require('dotenv').config()
+
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
+//const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/Workout-Tracker",
+    {
+        
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  console.log(process.env.MONGODB_URI)
