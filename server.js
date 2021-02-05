52d7b3798660b76ab67ebec282b3444cae9baff4
Nikstar-1 <rk_787@hotmail.co.uk>
2021-02-05 16:34:17 +0000

    set up .env, connected to mongo atlas, and heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 //require("./routes/apiRoutes")(app);
 //require("./routes/htmlRoutes")(app);
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/Workout-Tracker",
